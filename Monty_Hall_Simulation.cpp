dae6932f7cfef8f40ccb545880d329357710ddcf
Amit Dubey <35802447+amitdu6ey@users.noreply.github.com>
2019-03-31 19:44:17 +0530

    Update Monty_Hall_Simulation.cpp

--- Monty_Hall_Simulation.cpp
+++ Monty_Hall_Simulation.cpp
@@ -7,49 +7,49 @@
 int main(int argc, const char * argv[])
 {
     freopen("./Outputs/<insert_file_name_here>.txt","w",stdout);
-    int prize, choice, goat,totalTrials;    // choice is actually Gate selected. 
-    double win=0, loose=0;
+    int prize, door, goat, totalTrials;
+    double win = 0, loose = 0;
     srand(time(NULL));
-    for(totalTrials=10;totalTrials<=1000000000;totalTrials*=10){
-        win=0;
-        loose=0;
-        for(int i=0;i<totalTrials;i++){
+    for(totalTrials = 10;totalTrials <= 1000000000;totalTrials *= 10){
+        win = 0;
+        loose = 0;
+        for(int i = 0;i < totalTrials;i++){
 
             /*
-                below if we fix 'only' one of choice or Prize then also we will get same result.
-                choice=1;
+                below if we fix 'only' one of door or Prize then also we will get same result.
+                door=1;
                 prize=1;
             */
             // Randomly choosing Gate and Prize value
 
             prize = rand()%3;
-            choice = rand()%3;
-            
-            //one door is opened now which has Goat behind it.            
-            
-            for(int i=0;i<3;i++){
-                if(i!=choice&&i!=prize){
+            door = rand()%3;
+
+            //one door is opened now which has Goat behind it.
+
+            for(int i = 0;i < 3;i++){
+                if(i != door&&i != prize){
                     goat=i;
                     break;
                 }
             }
             /*
-             and now we will change our choice of Gate.
+             and now we will change our door of Gate.
             */
-            for(int i=0;i<3;i++){
-                if(i!=goat&&i!=choice){
-                    choice=i;
+            for(int i = 0;i < 3;i++){
+                if(i != goat&&i != door){
+                    door=i;
                     break;
                 }
             }
-            if(choice==prize){
-                win+=1;
+            if(door == prize){
+                win += 1;
             }
             else{
-                loose+=1;
+                loose += 1;
             }
         }
-        std::cout<<"for Total trials = "<<std::setfill('0')<<std::setw(10)<<totalTrials<<" Probability of winning if we switch our choice is "<<" = "<<(double)win/totalTrials<<std::endl;
+        std::cout<<"for Total trials = "<<std::setfill('0')<<std::setw(10)<<totalTrials<<" Probability of winning if we switch our door is "<<" = "<<(double)win/totalTrials<<std::endl;
     }
     return 0;
 }

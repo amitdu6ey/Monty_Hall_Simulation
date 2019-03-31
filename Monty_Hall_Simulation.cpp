08fa4e09204f681bcc76af9d0b497d51d1664295
Amit Dubey <35802447+amitdu6ey@users.noreply.github.com>
2019-03-31 19:19:20 +0530

    Update Monty_Hall_Simulation.cpp

--- Monty_Hall_Simulation.cpp
+++ Monty_Hall_Simulation.cpp
@@ -7,29 +7,26 @@
 int main(int argc, const char * argv[])
 {
     freopen("./Outputs/<insert_file_name_here>.txt","w",stdout);
-    int prize, choice, goat,totalTrials;    // choice is Gate choice
+    int prize, choice, goat,totalTrials;    // choice is actually Gate selected. 
     double win=0, loose=0;
     srand(time(NULL));
     for(totalTrials=10;totalTrials<=1000000000;totalTrials*=10){
-        win=0.0;
-        loose=0.0;
-        /*
-        below if we fix one of choice or prize then also we will get same result.
-        */
+        win=0;
+        loose=0;
         for(int i=0;i<totalTrials;i++){
 
             /*
-                We can fix value of choice and prize also
+                below if we fix 'only' one of choice or Prize then also we will get same result.
                 choice=1;
                 prize=1;
             */
-
             // Randomly choosing Gate and Prize value
+
             prize = rand()%3;
             choice = rand()%3;
-            /*
-            one door is opened now which has Goat behind it.
-            */
+            
+            //one door is opened now which has Goat behind it.            
+            
             for(int i=0;i<3;i++){
                 if(i!=choice&&i!=prize){
                     goat=i;
@@ -37,7 +34,7 @@ int main(int argc, const char * argv[])
                 }
             }
             /*
-             and now we will change our choice.
+             and now we will change our choice of Gate.
             */
             for(int i=0;i<3;i++){
                 if(i!=goat&&i!=choice){
@@ -52,7 +49,7 @@ int main(int argc, const char * argv[])
                 loose+=1;
             }
         }
-        std::cout<<"for Total trials = "<<std::setfill('0')<<std::setw(10)<<totalTrials<<" P(win) "<<" = "<<(double)win/totalTrials<<std::endl;
+        std::cout<<"for Total trials = "<<std::setfill('0')<<std::setw(10)<<totalTrials<<" Probability of winning if we switch our choice is "<<" = "<<(double)win/totalTrials<<std::endl;
     }
     return 0;
 }
